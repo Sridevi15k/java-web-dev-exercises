f1a2f64d28791d01c6bf1fd44cebe9c9c43bb889
Sridevi15k <sridevi15k@gmail.com>
2021-05-19 13:00:46 -0500

    lsn3/lsn4/lsn5Assignment-UnitTesting

---
+++ src/org/launchcode/java/demos/lsn3classes1/school/Teacher.java
@@ -0,0 +1,36 @@
+package org.launchcode.java.demos.lsn3classes1.school;
+
+public class Teacher {
+    private String firstName;
+    private String lastName;
+    private String subject;
+    private int yearsTeaching;
+
+
+    public void setFirstName(String FirstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String LastName) {
+        this.lastName = lastName;
+    }
+    public void setSubject(String Subject) {
+        this.subject = subject;
+    }
+    public void setYearsTeaching(int YearsTeaching) {
+        this.yearsTeaching = yearsTeaching;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getSubject() {
+        return subject;
+    }
+    public int getYearsTeaching() {
+        return yearsTeaching;
+    }
+
+}
--- src/org/launchcode/java/demos/lsn4classes2/Course.java
+++ src/org/launchcode/java/demos/lsn4classes2/Course.java
@@ -1,17 +1,70 @@
 package org.launchcode.java.demos.lsn4classes2;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Course {
-    private String topic;
-    private Teacher instructor;
-    private ArrayList<Student> enrolledStudents;
+    private static String topic;
+    private static Teacher instructor;
+    private static ArrayList<Student> enrolledStudents;
 
     // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather than
     //  just the class fields.
 
 
+    public static String getTopic() {
+        return topic;
+    }
+
+    public static void setTopic(String topic) {
+        Course.topic = topic;
+    }
+
+    public static Teacher getInstructor() {
+        return instructor;
+    }
+
+    public static void setInstructor(Teacher instructor) {
+        Course.instructor = instructor;
+    }
+
+    public static ArrayList<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public static void setEnrolledStudents(ArrayList<Student> enrolledStudents) {
+        Course.enrolledStudents = enrolledStudents;
+    }
+
+    @Override
+    public String toString() {
+        String  courseReport = String.format("Course info: ", this.topic, this.instructor, this.enrolledStudents);
+        return courseReport;
+
+    }
+
     // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
     //  Course objects equal.
 
+    @Override
+    public boolean equals(Object toBeCompared) {
+        if (toBeCompared == this) {
+            return true;
+        }
+        if (toBeCompared == null) {
+            return false;
+        }
+        if (toBeCompared.getClass() != getClass()) {
+            return false;
+        }
+        Course theCourse = (Course) toBeCompared;
+
+        return Objects.equals(topic, Course.topic) && Objects.equals(instructor, Course.instructor) && Objects.equals(enrolledStudents, Course.enrolledStudents);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(topic, instructor, enrolledStudents);
+    }
 }
